c97efe9bc01cf4a49498a387ee010af6cadd619b
Kangaechu <kangae2@gmail.com>
2019-11-30 13:47:50 +0900

    add version option

--- cmd/radiorenamer/main.go
+++ cmd/radiorenamer/main.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/kangaechu/radiorenamer"
+	"os"
 )
+
+var version string
+var revision string
 
 func main() {
 	flag.Parse()
+	if flag.Arg(0) == "version" {
+		fmt.Printf(os.Args[0]+": %s-%s\n", version, revision)
+		os.Exit(0)
+	}
+
 	radiorenamer.Run(flag.Arg(0))
 }
